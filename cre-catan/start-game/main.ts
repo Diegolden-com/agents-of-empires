8d30bb7cceecc1992cbe0dfb1a4e1d2fc64abfd1
Gilberts Ahumada <gilbertsahumada@gmail.com>
2025-11-22 20:54:41 -0300

    adding config to log trigger

--- cre-catan/start-game/main.ts
+++ cre-catan/start-game/main.ts
@@ -1,7 +1,14 @@
-import { cre, Runner, type Runtime } from "@chainlink/cre-sdk";
+import { cre, Runner, type Runtime, type EVMLog, bytesToHex, getNetwork } from "@chainlink/cre-sdk";
 
 type Config = {
   schedule: string;
+  chainSelectorName: string;
+  gameControllerAddress: string;
+};
+
+const onLogTrigger = (runtime: Runtime<Config>, log: EVMLog): string => {
+  runtime.log(`Log detected from ${log.address} chain`);
+  return "Log processed";
 };
 
 const onCronTrigger = (runtime: Runtime<Config>): string => {
@@ -11,6 +18,17 @@ const onCronTrigger = (runtime: Runtime<Config>): string => {
 
 const initWorkflow = (config: Config) => {
   const cron = new cre.capabilities.CronCapability();
+  const network = getNetwork({
+    chainFamily: "evm",
+    chainSelectorName: config.chainSelectorName,
+    isTestnet: true,
+  })
+
+  if(!network) {
+    throw new Error(`Network ${config.chainSelectorName} not found`);
+  }
+
+  const evmClient = new cre.capabilities.EVMClient(network.chainSelector.selector);
 
   return [
     cre.handler(
@@ -18,6 +36,12 @@ const initWorkflow = (config: Config) => {
         { schedule: config.schedule }
       ), 
       onCronTrigger
+    ),
+    cre.handler(
+      evmClient.logTrigger(
+        { addresses: [config.gameControllerAddress] }
+      ),
+      onLogTrigger
     ),
   ];
 };
